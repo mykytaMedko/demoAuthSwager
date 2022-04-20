44ff55b0c2c8db885c39f71bc08cd73453ac1b98
Nikita Medko <nikita.jove28@gmail.com>
2022-04-20 13:56:41 +0300

    Initial project. Database connection.

---
+++ src/main/java/com/example/demoauthswager/DemoAuthSwagerApplication.java
@@ -0,0 +1,14 @@
+package com.example.demoauthswager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAuthSwagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoAuthSwagerApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/com/example/demoauthswager/DemoAuthSwagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoauthswager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAuthSwagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
